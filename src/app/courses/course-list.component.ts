b0c22e32f78e859b20ad0109c8fc38a9fab62bc4
Raphael <rapha_rmm@hotmail.com>
2020-05-13 22:25:24 -0300

    add componentes na list

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
 
 @Component({
     selector: 'app-course-list',
     templateUrl: './course-list.component.html'
 })
-export class CourseListComponent{
+export class CourseListComponent implements OnInit{
+    courses: Course[] = [];
+
+    ngOnInit(): void {
+        this.courses = [
+            {
+                id: 1,
+                name: 'Angular: Forms',
+                imageUrl: '',
+                price: 99.99,
+                code: 'XPS-8796',
+                duration: 120,
+                rating: 4.5,
+                releaseDate: 'April, 8, 2020'
+            },
+            {
+                id: 2,
+                name: 'JavaScript: ES6',
+                imageUrl: '',
+                price: 88.99,
+                code: 'LKL-1094',
+                duration: 100,
+                rating: 4, 
+                releaseDate: 'May, 15, 2020'
+            }
+
+        ]
+    }
 
 }
